1a589b3c9518161092335e075a791f2902425497
ominna <iomelyukh@gmail.com>
2021-10-08 10:53:32 +0300

    Update string.py

--- string.py
+++ string.py
@@ -5,12 +5,13 @@ string_2 = str(input("Enter the second string: "))
 # check if string_2 is a substring of string_1
 def has_substring(s, t):
     if s == t:
-        return "String t is a substring of string s"
+        return "is a substring"
     else:
         for i in range(len(s) - len(t)+1):
             if s[i:i+len(t)] == t:
                 return "is a substring"
     return "is NOT a substring"
+
 print("\"%s\""%string_2, has_substring(string_1, string_2), "of", "\"%s\""%string_1)
 
 # check if the strings are numbers
