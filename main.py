9e973941ce574fadca7be521cf15def1975b28de
ominna <iomelyukh@gmail.com>
2021-10-08 10:50:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,22 @@
 
 import calc
 
-# Define operations
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
 # take input from the user
-select = int(input("Enter choice(1/2/3/4): "))
+expression = str(input("Enter your math expression in the format 'x operation y': "))
+my_list = expression.split()
+number_1 = my_list[0]
+number_2 = my_list[2]
 
-number_1 = int(input("Enter first number: "))
-number_2 = int(input("Enter second number: "))
-
-if select == 1:
+if my_list[1] == "+":
     print(number_1, "+", number_2, "=", calc.add(number_1, number_2))
 
-elif select == 2:
+elif my_list[1] == "-":
     print(number_1, "-", number_2, "=", calc.subtract(number_1, number_2))
 
-elif select == 3:
+elif my_list[1] == "*":
     print(number_1, "*", number_2, "=", calc.multiply(number_1, number_2))
 
-elif select == 4:
+elif my_list[1] == "/":
     print(number_1, "/", number_2, "=", calc.divide(number_1, number_2))
 else:
     print("Invalid input")
