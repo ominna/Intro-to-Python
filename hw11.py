c3965ced93cad448d901a665f4eaa1400137d6de
ominna <iomelyukh@gmail.com>
2021-10-22 20:46:19 +0300

    Update hw11.py

--- hw11.py
+++ hw11.py
@@ -10,8 +10,9 @@ class Point:
         r = str(round(math.sqrt(self.x * self.x + self.y * self.y),2))   # radius
         theta = math.atan(self.y / self.x)                               # angle in radians
         theta = 180 * theta / math.pi                                    # angle in degrees
-        print(f"The Polar coordinates are: r={r}; theta={theta}")
+        result = (r, theta)
+        return result
 
 # Checking on a point
 p1 = Point(x=1, y=1)
-p1.to_polar()
+print(p1.to_polar())
